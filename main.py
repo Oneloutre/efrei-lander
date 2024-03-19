f7a1b8f5fcad5d8ebea85f085a987c10891acffa
rayanekaraouzene <rayane.karaouzene@efrei.net>
2024-03-19 13:34:45 +0100

    hi

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ size_x = 1280
 size_y = 720
 screen = pygame.display.set_mode((size_x, size_y))
 pygame.display.set_caption("Efrei Lander")
+background_img = pygame.image.load("Assets/mainmenu.png")
+background_img = pygame.transform.scale(background_img, (size_x, size_y))
 background_color = (0, 0, 0)
 
 running = True
-screen.fill(background_color)
+screen.blit(background_img,(0,0))
 x1, x2, largeur_plateforme = generer_plateforme(screen)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     pygame.display.update()
 
 pygame.quit()
