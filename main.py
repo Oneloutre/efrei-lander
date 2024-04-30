d73a0f94966f64fae2dc7be046d7c99c388bba0b
rayanekaraouzene <rayane.karaouzene@efrei.net>
2024-04-30 17:59:59 +0200

    rayane graphic update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from functions import *
 import pygame
 
 pygame.init()
+#physics
+fuel = 100
 
 # Création de la fenêtre de jeu
 size_x = 1280
@@ -9,6 +11,7 @@ size_y = 720
 screen = pygame.display.set_mode((size_x, size_y))
 pygame.display.set_caption("Efrei Lander")
 background_image = pygame.image.load("Assets/space.png").convert()
+fuel_image = pygame.image.load("Assets/fuel.png").convert_alpha()
 
 # Coordonnées de la lune
 x_lune = size_x - 100
@@ -20,10 +23,11 @@ rayon_lune = 50
 image_lune = pygame.image.load('Assets/planet.png').convert_alpha()
 image_lune = pygame.transform.scale(image_lune, (rayon_lune*2, rayon_lune*2))  # Ajuster la taille de l'image
 
-
+font = pygame.font.Font("Assets/ethnocentric rg.otf",16)
 
 running = True
 screen.blit(background_image, (0, 0))
+screen.blit(fuel_image,(40,40))
 x1, x2, y1, y2, hauteur_plateforme = generer_plateforme(screen)
 generer_montagne(screen, x1, x2, y1, y2)
 generer_montagne(screen, x2, 1280, y2, 650)
@@ -41,6 +45,12 @@ while running:
             running = False
     pygame.display.update()
     screen.blit(image_lune, (x_lune - rayon_lune, y_lune - rayon_lune))
+    text_fuel = font.render(str(fuel),True,(255,255,255))
+    fuel_rect = text_fuel.get_rect()
+
+    fuel_rect.topleft = (140,60)
+
+    screen.blit(text_fuel,fuel_rect)
 
 
 pygame.quit()
