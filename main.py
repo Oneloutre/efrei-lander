005bb820403a7fb2cfb5ac84118a76fdb70503fc
rayanekaraouzene <rayane.karaouzene@efrei.net>
2024-05-14 23:02:58 +0200

    Enhanched graphics

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from functions import *
 import pygame
+import sys
+import math
 
 pygame.init()
 
 
 #physics
 fuel = 100
+move_x = 0
+velocity_x = 0
+velocity_y = 0
+gravity = 0.1
+angle = 0
+speed = 5
+user_impact = 0
+right_pressed = False
+left_pressed = False
+up_pressed = False
+running = True
 clock = pygame.time.Clock()
 
 #controls
@@ -27,6 +40,11 @@ y_lune = 100
 rayon_lune = 50
 image_lune = pygame.image.load('Assets/planet.png').convert_alpha()
 
+#lander
+icon = pygame.image.load("Assets/lander.png")
+icon_rect = icon.get_rect()
+icon_rect.center = (size_x // 2, size_y // 2)
+
 #text
 font = pygame.font.Font("Assets/ethnocentric rg.otf",16)
 
@@ -39,39 +57,73 @@ print(mountain_coords)
 #game
 while running:
 
-    clock.tick(60)
+
     screen.blit(background_image, (0, 0))
-    screen.blit(fuel_image, (40, 40))
+
     screen.blit(image_lune, (x_lune - rayon_lune, y_lune - rayon_lune))
-    text_fuel = font.render(str(fuel), True, (255, 255, 255))
-    fuel_rect = text_fuel.get_rect()
-    fuel_rect.topleft = (140, 60)
-    screen.blit(text_fuel, fuel_rect)
+
     dessiner_plateforme(x1, x2, y1, y2, hauteur_plateforme,screen)
     generer_montagne(screen, x1, x2, y1, y2)
     generer_montagne(screen, x2, 1280, y2, 650)
     generer_montagne(screen, 0 - largeur_plateforme, x1, 500, y1)
+
     fill_mountain(screen)
+    rotated_icon = pygame.transform.rotate(icon, angle)
+    rotated_rect = rotated_icon.get_rect(center=icon_rect.center)
+
+    screen.blit(fuel_image, (40, 40))
+    screen.blit(rotated_icon, rotated_rect)
+    text_fuel = font.render(str(fuel), True, (255, 255, 255))
+    fuel_rect = text_fuel.get_rect()
+    fuel_rect.topleft = (140, 60)
+    screen.blit(text_fuel, fuel_rect)
+
+
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_z:
+            if event.key == pygame.K_RIGHT:
+                right_pressed = True
+            elif event.key == pygame.K_LEFT:
+                left_pressed = True
+            elif event.key == pygame.K_UP:
                 z_pressed = True
+                up_pressed = True
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_z:
+            if event.key == pygame.K_RIGHT:
+                right_pressed = False
+            elif event.key == pygame.K_LEFT:
+                left_pressed = False
+            elif event.key == pygame.K_UP:
                 z_pressed = False
+                up_pressed = False
 
     if z_pressed and fuel !=0:
         fuel-=1
         pygame.time.delay(100)
     pygame.display.update()
+    if left_pressed:
+        angle += 1
+    if right_pressed:
+        angle -= 1
+
+    if up_pressed:
+        velocity_x = -speed * math.sin(math.radians(angle))
+        velocity_y = -speed * math.cos(math.radians(angle))
+        print(math.sin(math.radians(angle)))
+        user_impact = 0.2
+
+    velocity_y += gravity
+    icon_rect.x += velocity_x
+    icon_rect.y += velocity_y
 
 
 
 
+    clock.tick(60)
     pygame.display.flip()
 
 
